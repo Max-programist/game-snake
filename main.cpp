fb352388555ee4b4dfbe1120dbe47bca321dfc27
Max <makskiller1511@gmail.com>
2025-12-26 17:27:43 +0300

    snake can move

--- main.cpp
+++ main.cpp
@@ -36,27 +36,45 @@ void border(std::vector<std::vector<char>>& arr) {
     }
 }
 
+void clean_map(std::vector<std::vector<char>>& arr) {
+    for(int i = 0; i < 20; i++) {
+        for(int j = 0; j < 20; j++) {
+            arr[i][j] = '.';
+        }
+    }
 
+}
 
 
 int main() {
     std::vector<std::vector<char>> arr = create();
     border(arr);
     MyList example;
-    example.add_back(6, 6);
-    example.add_back(7, 6);
-    example.add_back(8, 6);
+    example.add_front(6, 6);
+    example.add_front(7, 6);
+    example.add_front(8, 6);
     example.draw_snake(arr);
     print(arr);
 
 
     for(; ;) {
         char c = getchar();
-        std::cout << "log getchar \n";
+        std::cout << "log getchar " << c << "\n";
         if(c == 'q') {
-            break;
-       } 
-
+            break;    
+    //    } else if(c == "w") {
+    
+    //    } 
+        } else {
+            
+            example.add_front(9, 6);
+            example.pop_back(); //FIIX MEEEEEEEEEEEEEEEEEEEEEEEEE
+            example.show();
+       }
+       clean_map(arr);
+       border(arr);
+       example.draw_snake(arr);
+       print(arr);
     }
 
     return 0;
